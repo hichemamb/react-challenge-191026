0a2199899f1e5c3db3ecebc05510487be6df339b
Thomas Deruel <thomas.deruel@hetic.net>
2019-10-31 23:18:20 +0100

    [back](controllers/list.js) list created

--- server/controllers/list.js
+++ server/controllers/list.js
@@ -1,5 +1,10 @@
 const db = require('../config/db');
 
 exports.getStudentsList = (req, res) => {
-
+    db.users.findAll({
+        attributes:['id','firstname','lastname','email','promotion']
+    })
+    .then(users=>{
+        res.json(users)
+    })
 };
