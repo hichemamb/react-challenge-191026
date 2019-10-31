0b541a71d8941279b4bdf346157e093eec02609b
Hichem AMAR BENSABER <amar-bensaber.h@sfeir.com>
2019-10-31 14:40:17 +0100

    [RC](feats): Add Register/Login to DB endpoints / Establish passport to protect futur endpoints and to establish authentication

--- server/config/db.js
+++ server/config/db.js
@@ -4,9 +4,6 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize(env.DATABASE_NAME, env.DATABASE_USERNAME, env.DATABASE_PASSWORD, {
    host: env.DATABASE_HOST,
    dialect: env.DATABASE_DIALECT,
-   define: {
-      underscored: true
-   }
 });
 
 const db = {};
--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,16 +1,63 @@
 const db = require('../config/db');
+const jwt = require('jsonwebtoken');
+const env = require('../config/env');
 
-exports.register = async (req, res) => {
+exports.register = (req, res) => {
+
    const {firstname, lastname, email, promotion, description, password, skills} = req.body;
-   db.users.create({
-      firstname,
-      lastname,
-      email,
-      promotion,
-      description,
-      password,
-      skills,
-   }, {
-      include: [db.skills]
-   }).then(data => res.send(data));
+
+   if (!email || !password || !firstname || !lastname) {
+      res.send('Invalid details');
+   } else {
+      db.users.findOne({where: {email}})
+         .then(user => {
+            if (user === null) {
+               db.users.create({
+                  firstname,
+                  lastname,
+                  email,
+                  promotion,
+                  description,
+                  password,
+                  skills,
+               }, {
+                  include: [db.skills]
+               })
+                  .then(user => res.send(user))
+                  .catch((err) => {
+                     console.log('Failed to inserted data');
+                     console.log(err);
+                  });
+            } else {
+               res.send('User with same email already exist');
+            }
+         });
+   }
+};
+
+exports.login = (req, res) => {
+
+   const {email, password} = req.body;
+
+   if (!email || !password) {
+      res.send('Invalid details');
+   } else {
+      db.users.findOne({where: {email}})
+         .then(user => {
+               if (user !== null && user.email === email && user.password === password) {
+                  const payload = {
+                     id: user.id
+                  };
+
+                  jwt.sign(payload, env.SECRET_OR_KEY, {expiresIn: 31556926}, (err, token) => {
+                     res.json({success: true, token: token});
+                  });
+
+               } else {
+                  res.status(400).json({incorrect: 'Incorrect email or password, retry'})
+                  ;
+               }
+            }
+         );
+   }
 };
