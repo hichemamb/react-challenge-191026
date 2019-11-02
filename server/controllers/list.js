210393ca72f0d58a2ca6cd4a0d2d510a7c6e0e07
Thomas Deruel <thomas.deruel@hetic.net>
2019-11-02 15:51:19 +0100

    [back](constrollers/list.js): include skills

--- server/controllers/list.js
+++ server/controllers/list.js
@@ -2,7 +2,8 @@ const db = require('../config/db');
 
 exports.getStudentsList = (req, res) => {
     db.users.findAll({
-        attributes:['id','firstname','lastname','email','promotion']
+        attributes:['id','firstname','lastname','email','promotion'],
+        include:[db.skills]
     })
     .then(users=>{
         res.json(users)
