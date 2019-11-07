95f7280ecd498dfe348866f0560dd1ece33cba97
Thomas Deruel <thomas.deruel@hetic.net>
2019-11-07 16:24:24 +0100

    [RC](component update): add description student on StudentView

--- src/components/StudentView/StudentView.js
+++ src/components/StudentView/StudentView.js
@@ -12,13 +12,14 @@ const studentData = {
     picture: "http://teenlife.blogs.pressdemocrat.com/files/2010/09/codding.jpg",
     birthday: '08/12/1996',
     email: 'Sarah.Vidal@hetic.net',
-    skills: ['CSS', 'HTML']
+    skills: ['CSS', 'HTML'],
+    description: '',
 }
 
-const fullName = studentData.firstname +' '+ studentData.lastname;
+const fullName = studentData.firstname + ' ' + studentData.lastname;
 
-const SkillsList = () => studentData.skills.map((skill,id)=> <li key={id}>{skill}</li>)
-
+const SkillsList = () => studentData.skills.map((skill, id) => <li key={id}>{skill}</li>)
+const haveADescription = () => studentData.description === '' ? false : true;
 const StudentView = () => {
     return (
         <div>
@@ -45,16 +46,23 @@ const StudentView = () => {
                                 <div className="studentview-profil-container-moreinfo-skills">
                                     <h2>Compétences</h2>
                                     <ul>
-                                        <SkillsList/>
+                                        <SkillsList />
                                     </ul>
                                 </div>
                             </div>
                         </div>
                     </section>
                 </Card>
+                <Card title="Cursus" width="75%">
+                    {
+                        haveADescription ?
+                            <p style={{opacity:0.5}}>Aucune déscription pour le moment</p> :
+                            <p>description</p>
+                    }
+                </Card>
             </div>
             <div className="studentview_btn">
-                <ButtonBack/>
+                <ButtonBack />
             </div>
         </div>
     )
