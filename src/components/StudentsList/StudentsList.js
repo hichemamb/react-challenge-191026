7050cf8ba6d5d0837e4fdb60ab7b6bd7ac2cb750
ThomasFranja <thomas.franja@hetic.net>
2019-11-08 01:56:08 +0100

    [RC](resolve #17): props static created for name ItemStudents + StudentsList dynamic components modified

--- src/components/StudentsList/StudentsList.js
+++ src/components/StudentsList/StudentsList.js
@@ -7,12 +7,25 @@ import ItemStudents from '../_shared/ItemStudents/ItemStudents';
 
 
 const StudentsList = () => {
+   const studentsName = ['Nancy Campbell','Sofia Hanna','Nino Lam','Maxime Charpentier','Mathieu Blok','Jay Dogo','Joe Texeira','Antoine Beaudoire','Thomas Franja', 'Hichem AMAR BENSABER', 'Thomas Deruel'];
    return (
       <div>
          <Header/>
          <section className="students">
             <h1 className="students__title"><strong>Liste des étudiants</strong></h1>
-            <ItemStudents/>
+            <div className="studentsItem">
+               <ul>
+               {studentsName.map((name, alt) => {
+                  return (
+                     <a href="/" className="studentsItem__link">
+                        <li className="studentsItem__list">
+                           <ItemStudents name={name} alt={alt}/>   
+                        </li>
+                     </a>
+                  )
+               })}
+               </ul>
+            </div>
          </section>
       </div>
    );
--- src/components/_shared/ItemStudents/ItemStudents.js
+++ src/components/_shared/ItemStudents/ItemStudents.js
@@ -2,24 +2,11 @@ import React from 'react';
 import './ItemStudents.scss';
 import IconUser from '../../../assets/img/icon-user.svg';
 
-const ItemStudents = () => {
-   const studentsName = ['Nancy Campbell','Sofia Hanna','Nino Lam','Maxime Charpentier','Mathieu Blok','Jay Dogo','Joe Texeira','Antoine Beaudoire','Thomas Franja', 'Hichem AMAR BENSABER', 'Thomas Deruel'];
+const ItemStudents = ({name, alt}) => {
    return (
        <div>
-         <div className="studentsItem">
-            <ul>
-               {studentsName.map((value, index) => {
-                  return (
-                     <a href="/" className="studentsItem__link">
-                        <li className="studentsItem-list">
-                           <img className="studentsItem-list__icon" src={IconUser}/>
-                           <span className="studentsItem-list__name" key={index}>{value}</span>
-                        </li>
-                     </a>
-                  )
-               })}
-            </ul>
-         </div>
+            <img className="studentsItem__icon" src={IconUser} alt={alt}/>
+            <span className="studentsItem__name"> {name} </span>
        </div>
    );
 };
