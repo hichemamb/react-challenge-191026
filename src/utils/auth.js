4b1841a30b6566dadaee300e8e7fdcf4ae212501
Thomas Deruel <thomas.deruel@hetic.net>
2019-11-11 19:41:02 +0100

    [RC](new instance): add auth

---
+++ src/utils/auth.js
@@ -0,0 +1,21 @@
+class Auth {
+    constructor() {
+        this.authentification = localStorage.getItem('token') || false;
+    }
+
+    setToken(newToken) {
+        this.authentification = localStorage.setItem('token', JSON.stringify(newToken));
+    }
+
+    getToken() {
+        // add you're token with postman ;)
+        return 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImUzYzgxMTkyLTg0ZDUtNDQxOS04ZmViLTNhYmY4ZGM2N2U2YyIsImlhdCI6MTU3MzQ4MTI2OSwiZXhwIjoxNjA1MDM4MTk1fQ.-rTJZoVOwHZdfwhEiHG-aMRxdz32dvl2z-no8d0iObg'
+        //return this.authentification;
+    }
+
+    removeToken() {
+        this.authentification = null;
+        localStorage.removeItem('token');
+    }
+}
+export default new Auth();
