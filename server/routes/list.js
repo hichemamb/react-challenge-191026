5004cebc54331b7523db8100c432a9932e3ae5ee
Thomas Deruel <thomas.deruel@hetic.net>
2019-11-11 15:23:05 +0100

    [RC](new request): GET /list/:id

--- server/routes/list.js
+++ server/routes/list.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const list = require('../controllers/list');
 const passport = require('passport');
 
+router.get('/list/:id',  passport.authenticate('jwt', { session: false }), list.getStudentById);
 router.get('/list',  passport.authenticate('jwt', { session: false }), list.getStudentsList);
 
 module.exports = router;
