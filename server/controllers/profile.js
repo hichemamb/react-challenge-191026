98712c637d4e6c424981daf3367e467b32110bf0
Thomas Deruel <thomas.deruel@hetic.net>
2019-11-13 23:31:06 +0100

    [RC](new feature): create new skills if unexist

--- server/controllers/profile.js
+++ server/controllers/profile.js
@@ -1,39 +1,53 @@
 const db = require('../config/db');
 
 exports.getUserInfos = (req, res) => {
-   const {id} = req.body
-   db.users.findOne({where: {id}, include: [db.skills]})
+   const { id } = req.body
+   db.users.findOne({ where: { id }, include: [db.skills] })
       .then(user => {
          res.json(user);
       });
 };
 
 exports.putUserInfos = (req, res) => {
-   const {id} = req.body;
-   db.users.findOne({where: {id}, include: [db.skills]})
-      .then(user => { 
+   const { id } = req.body;
+   db.users.findOne({ where: { id }, include: [db.skills] })
+      .then(user => {
          const parametersBody = Object.keys(req.body);
          const parametersUser = Object.keys(user.dataValues);
          // check if all keys exist
-         const everyKeysAreOk = parametersBody.every( p => parametersUser.includes(p))
+
+         const everyKeysAreOk = parametersBody.every(p => parametersUser.includes(p))
          try {
-            if(!everyKeysAreOk){
+            if (!everyKeysAreOk) {
                throw "parameters invalid, please retry"
             }
+            // check if I have new skills
+            const newSkills = req.body.skills.filter(skill => !skill.id);
+            if (newSkills.length) {
+               let promises = []
+               for (const skill of newSkills) {
+                  promises.push(db.skills.create({
+                     skill: skill.skill,
+                     mark: skill.mark,
+                     userId: id
+                  }))
+               }
+               Promise.all(promises).catch(err => console.log('error:', err))
+            }
             user.update(req.body)
-               .then(updateUser =>{
+               .then(updateUser => {
                   res.json(updateUser)
                })
-               .catch(error =>{
-                  res.status(400).json({incorrect: 'error update, please retry',error})
+               .catch(error => {
+                  res.status(400).json({ incorrect: 'error update, please retry', error })
                })
          }
-         catch(err){
-            const listKeysUndefined = parametersBody.filter( p => !parametersUser.includes(p))
-            res.status(400).json({incorrect: err, error:`this element(s) is/are undefined: ${listKeysUndefined.join(' && ')}`})
+         catch (err) {
+            const listKeysUndefined = parametersBody.filter(p => !parametersUser.includes(p))
+            res.status(400).json({ incorrect: err, error: `this element(s) is/are undefined: ${listKeysUndefined.join(' && ')}` })
          }
       })
-      .catch(error=>{
-         res.status(400).json({incorrect: 'unknown id, please retry',error})
+      .catch(error => {
+         res.status(400).json({ incorrect: 'unknown id, please retry', error })
       })
 };
