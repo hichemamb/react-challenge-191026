d2e000a69f7480b3562caab452eefe1f378e71a5
Hichem AMAR BENSABER <amar-bensaber.h@sfeir.com>
2019-11-13 09:38:43 +0100

    [RC](feat): Added docker (dockerfile for front and back) / docker-compose.yml

--- server/config/env.js
+++ server/config/env.js
@@ -1,9 +1,9 @@
 module.exports = {
-   PORT: 8080,
-   DATABASE_NAME: 'challenge_hetic_list',
-   DATABASE_USERNAME: 'root',
-   DATABASE_PASSWORD: 'password',
-   DATABASE_HOST: 'localhost',
-   DATABASE_DIALECT: 'mysql',
+   PORT: process.env.PORT || 3000,
+   DB_NAME: process.env.DB_NAME || 'challenge_hetic_list',
+   DB_USERNAME: process.env.DB_USER || 'user',
+   DB_PASSWORD: process.env.DB_PASSWORD || 'pass',
+   DB_HOST: process.env.DB_HOST || 'localhost',
+   DB_DIALECT: 'mysql',
    SECRET_OR_KEY: 'hichkey'
 };
