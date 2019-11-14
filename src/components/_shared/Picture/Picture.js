34992c60bbe94f63d6dfaa52588bd7b03e92f159
Thomas Deruel <thomas.deruel@hetic.net>
2019-11-14 19:33:43 +0100

    [RC](fix component): fix picture and header style

--- src/components/_shared/Picture/Picture.js
+++ src/components/_shared/Picture/Picture.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import "./Picture.scss";
-
-const Picture = ({ src, alt = "picture", width = 100, height = 100 }) => {
+import img from "../../../assets/img/hetic-logo.png"
+const Picture = ({ src="", alt = "picture", width = 100, height = 100 }) => {
     const style = { width, height }
+    const img = src === "" ? require("../../../assets/img/default-user.svg") : src;
     return (
         <div className="picture" style={{...style}}>
             <div className="picture-container">
-                <img src={src} alt={alt} />
+                <img src={img} alt={alt} />
             </div>
         </div>
     )
