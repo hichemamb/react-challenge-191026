306adc2d673d7fc24d4d8981b128cc31f8afb062
ThomasFranja <thomas.franja@firmfunding.com>
2020-01-09 19:21:18 +0100

    [RC](feats): Proto that we added to the mobile folder react native

--- mobile/App.js
+++ mobile/App.js
@@ -1,19 +1,104 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Image, ScrollView, SafeAreaView } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+const data = [
+  {
+    id: 1,
+    name: "Thomas DERUEL",
+    age: 25,
+    img: "https://cdn.shopify.com/s/files/1/0246/4622/1903/products/legostarwars-display_300x300.png?v=1578179637"
+  },
+  {
+    id: 2,
+    name: "Thomas FRANJA",
+    age: 29,
+    img: "https://cdn.shopify.com/s/files/1/0246/4622/1903/products/legostarwars-display_300x300.png?v=1578179637"
+  },
+  {
+    id: 4,
+    name: "Hichem AMAR B",
+    age: 21,
+    img: "https://cdn.shopify.com/s/files/1/0246/4622/1903/products/legostarwars-display_300x300.png?v=1578179637"
+  },
+  {
+    id: 5,
+    name: "Hichem AMAR B",
+    age: 21,
+    img: "https://cdn.shopify.com/s/files/1/0246/4622/1903/products/legostarwars-display_300x300.png?v=1578179637"
+  },
+  {
+    id: 6,
+    name: "Hichem AMAR B",
+    age: 21,
+    img: "https://cdn.shopify.com/s/files/1/0246/4622/1903/products/legostarwars-display_300x300.png?v=1578179637"
+  },
+  {
+    id: 7,
+    name: "Hichem AMAR B",
+    age: 21,
+    img: "https://cdn.shopify.com/s/files/1/0246/4622/1903/products/legostarwars-display_300x300.png?v=1578179637"
+  },
+  {
+    id: 8,
+    name: "Hichem AMAR B",
+    age: 21,
+    img: "https://cdn.shopify.com/s/files/1/0246/4622/1903/products/legostarwars-display_300x300.png?v=1578179637"
+  },
+  {
+    id: 9,
+    name: "Hichem AMAR B",
+    age: 21,
+    img: "https://cdn.shopify.com/s/files/1/0246/4622/1903/products/legostarwars-display_300x300.png?v=1578179637"
+  },
+]
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fff',
+    alignItems: 'center',
+  },
+  flex: {
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    textAlign: 'left'
+  },
+  item: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    padding: 12,
+  },
+  image: {
+    width:60,
+    height:60,
+  }
+});
+const Item = ({item}) => {
+  return(
+    <View style={styles.item}>
+      <Image resizeMode='cover' style={styles.image} source={{uri: "https://cdn.shopify.com/s/files/1/0246/4622/1903/products/legostarwars-display_300x300.png?v=1578179637"}} />
+      <View style={styles.flex}>
+        <Text>{item.name}</Text>
+        <Text> {item.age} ans</Text>
+      </View>
     </View>
+      )
+    }
+const idExtractor = (item) => item.id;
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <View style={styles.container}>
+        <FlatList 
+        data={data} 
+        renderItem={Item}
+        keyExtractor={idExtractor}
+        />
+        </View>
+      </ScrollView>  
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+export default App;
